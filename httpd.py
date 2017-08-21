e6ec4c46746e8e207b4a713b7ca1c6ffbc6b040c
李可 <like@wesai.com>
2017-08-21 16:52:25 +0800

    遥控

---
+++ httpd.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
+import cgi
+#import L298N_car3
+
+PORT_NUMBER = 8080
+
+# 开车命令的频率多少合适呢？
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+    #Handler for the GET requests
+    def do_GET(self):
+        #print self.path
+        if self.path=="/":
+            self.path="/httpd.html"
+
+        try:
+            #Check the file extension required and
+            #set the right mime type
+
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype='text/html'
+                sendReply = True
+            if self.path.endswith(".jpg"):
+                mimetype='image/jpg'
+                sendReply = True
+            if self.path.endswith(".gif"):
+                mimetype='image/gif'
+                sendReply = True
+            if self.path.endswith(".js"):
+                mimetype='application/javascript'
+                sendReply = True
+            if self.path.endswith(".css"):
+                mimetype='text/css'
+                sendReply = True
+
+            if sendReply == True:
+                #Open the static file requested and send it
+                f = open(curdir + sep + self.path) 
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    #Handler for the POST requests
+    def do_POST(self):
+        if self.path=="/command":
+            form = cgi.FieldStorage(
+                fp=self.rfile, 
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+            })
+
+            command = form["command"].value
+
+            if command == "forward":
+                #L298N_car3.go(0.5)
+                print "command is: %s" % command
+            if command == "backward":
+                #L298N_car3.back(0.5)
+                print "command is: %s" % command
+            if command == "left":
+                #L298N_car3.left(0.5)
+                print "command is: %s" % command
+            if command == "right":
+                #L298N_car3.right(0.5)
+                print "command is: %s" % command
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write("Let's %s !" % command)
+            return          
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
+
